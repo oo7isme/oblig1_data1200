a3127fce70f827db64a5877de04beba44f0a91f6
Patchayaw04 <patchaya1@outlook.co.th>
2025-10-06 13:25:59 +0200

    main files

---
+++ index.js
@@ -0,0 +1,22 @@
+const ifmTobias = document.getElementById("tobias") 
+const ifmPatch = document.getElementById("patchaya")  
+const ifmEmil = document.getElementById("emil")  
+const ifmAlex = document.getElementById("alexander")  
+const ifmHaroon = document.getElementById("haroon")   
+
+function toggle(elementId){
+    const element = document.getElementById(elementId);
+    ifmTobias.style.display = "none";
+    ifmPatch.style.display = "none";
+    ifmEmil.style.display = "none";
+    ifmAlex.style.display = "none";
+    ifmHaroon.style.display = "none";
+    
+    if(element.style.display === 'none'){
+        element.style.display = "block";
+    } 
+    else {
+        element.style.display = "none";
+    }
+    console.log("toggle")
+}
